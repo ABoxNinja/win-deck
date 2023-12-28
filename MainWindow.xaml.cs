04aa4062c849758e6cd52c8631b7e3cf5990fc89
aboxninja <quexfp@gmail.com>
2023-12-28 22:43:19 +0200

    created deck

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace deck
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowCentered(object sender, EventArgs e)
+        {
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double formWidth = this.ActualWidth;
+
+            double x = (screenWidth - formWidth) / 2;
+            double y = 0;
+
+            this.Left = x;
+            this.Top = y;
+        }
+    }
+}
