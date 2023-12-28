76b12a1548efa9e40b8867c3509a4a0d1eea260f
aboxninja <quexfp@gmail.com>
2023-12-28 23:43:45 +0200

    added CPU usage

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,12 +6,14 @@ namespace deck
 {
     public partial class MainWindow : Window
     {
+        PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
         public MainWindow()
         {
             InitializeComponent();
             this.Width = SystemParameters.PrimaryScreenWidth;
 
             UpdateRam();
+            UpdateCPU();
 
             System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
             timer.Tick += Timer_Tick;
@@ -19,9 +21,10 @@ namespace deck
             timer.Start();
         }
         
-        private void Timer_Tick(object sender, EventArgs e)
+        private void Timer_Tick(object? sender, EventArgs e)
         {
             UpdateRam();
+            UpdateCPU();
         }
 
         private void UpdateRam()
@@ -30,6 +33,15 @@ namespace deck
             long ramUsage = process.WorkingSet64;
             double ramUsageMB = ramUsage/(1024.0*1024.0);
             ramUsageLabel.Content = $"RAM: {ramUsageMB:F2} MiB";
+        }
+
+        private void UpdateCPU()
+        {
+            Thread.Sleep(100);
+            float cpuUsage = cpuCounter.NextValue();
+            Thread.Sleep(100);
+            cpuUsage = (float)Math.Round(cpuUsage,2);
+            cpuUsageLabel.Content = $"CPU: {cpuUsage}%";
         }
         
         private void WindowCentered(object sender, EventArgs e)
